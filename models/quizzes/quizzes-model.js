63c6c95d8b6cf984dff80e0839f6448eda332fc5
Eshwari Bhide <ebhide99@gmail.com>
2021-04-16 16:23:07 -0700

    adding quiz attempts

--- models/quizzes/quizzes-model.js
+++ models/quizzes/quizzes-model.js
@@ -1,6 +1,6 @@
 const quizzesSchema = require("./quizzes-schema")
 const mongoose = require("mongoose")
 const quizzesModel = mongoose.model(
-"QuizModel",
+"QuizzesModel",
 quizzesSchema)
 module.exports = quizzesModel
