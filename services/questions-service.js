aee7cbf0e2a70fc27505c65e17b0b6c71590f0d3
Eshwari Bhide <ebhide99@gmail.com>
2021-04-16 16:16:07 -0700

    adding services

--- services/questions-service.js
+++ services/questions-service.js
@@ -1,21 +1,25 @@
 const questions = require("./questions.json")
+const questionsModel = require("../models/questions/questions-model.js")
 
 const createQuestion = () => {}
 const createQuestionForQuiz = () => {}
 
 const findAllQuestions = () => {
-    return questions
+    return questionsModel.find()
+    //return questions
 }
 
 const findQuestionsForQuiz = (qzid) => {
-    return questions.filter((question) => {
-        return question.quizId === qzid;
-    })
+    return questionsModel.find({quizId: qzid})
+//    return questions.filter((question) => {
+//        return question.quizId === qzid;
+//    })
 }
 const findQuestionById = (quid) => {
-    return questions.find((question) => {
-        return question._id === quid;
-    })
+    return questionsModel.findById(quid)
+//    return questions.find((question) => {
+//        return question._id === quid;
+//    })
 }
 
 const updateQuestion = () => {}
--- services/quiz-service.js
+++ services/quiz-service.js
@@ -1,13 +1,17 @@
 const quizzes = require("./quizzes.json")
 
+const quizzesModel = require("../models/quizzes/quizzes-model")
+
 const createQuiz = () => {}
 const findAllQuizzes = () => {
-    return quizzes
+    return quizzesModel.find()
+    //return quizzes
 }
 const findQuizById = (qid) => {
-    return quizzes.find((quiz) => {
-        return (quiz._id === qid)
-    })
+    return quizzesModel.findById(qid)
+//    return quizzes.find((quiz) => {
+//        return (quiz._id === qid)
+//    })
 }
 const updateQuiz = () => {}
 const deleteQuiz = () => {}
