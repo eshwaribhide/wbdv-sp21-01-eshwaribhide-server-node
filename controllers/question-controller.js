9af5f36c9a0c58037ce2cfa6d0be68e43a32ded9
Eshwari Bhide <ebhide99@gmail.com>
2021-04-16 16:15:51 -0700

    editing question and quizzes controllers

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -2,14 +2,17 @@ module.exports = (app) => {
     const questionService = require("../services/questions-service")
 
     const findAllQuestions = (req, res) => {
-        const questions = questionService.findAllQuestions()
-        res.send(questions)
+//        const questions = questionService.findAllQuestions()
+//        res.send(questions)
+    questionService.findAllQuestions().then((questions => {res.send(questions)}))
     }
 
     const findQuestionsForQuiz = (req, res) => {
-        const quizId = req.params.qid;
-        const questions = questionService.findQuestionsForQuiz(quizId);
-        res.send(questions);
+    questionService.findQuestionsForQuiz(quizId).then((questions) => {res.send(questions)})
+
+//        const quizId = req.params.qid;
+//        const questions = questionService.findQuestionsForQuiz(quizId);
+//        res.send(questions);
     }
 
     app.get("/api/questions", findAllQuestions);
--- controllers/quizzes-controller.js
+++ controllers/quizzes-controller.js
@@ -2,13 +2,19 @@ const quizzesService = require("../services/quiz-service")
 
 module.exports = (app) => {
     const findAllQuizzes = (req, res) => {
-        const quizzes = quizzesService.findAllQuizzes()
-        res.send(quizzes)
+//        const quizzes = quizzesService.findAllQuizzes()
+//        res.send(quizzes)
+    quizzesService.findAllQuizzes()
+    .then((quizzes) => {res.send(quizzes)
+    })
     }
     const findQuizById = (req, res) => {
-        const quizId = req.params['qid']
-        const quiz = quizzesService.findQuizById(quizId)
-        res.send(quiz)
+//        const quizId = req.params['qid']
+//        const quiz = quizzesService.findQuizById(quizId)
+        quizzesService.findQuizById(quizId)
+        .then((quiz) => {res.send(quizzes)
+        })
+
     }
 
     app.get("/api/quizzes", findAllQuizzes)
