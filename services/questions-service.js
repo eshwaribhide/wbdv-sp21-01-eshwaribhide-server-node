89f1288bada4f1c189ed5a0a13cc47fbf92293a5
Eshwari Bhide <ebhide99@gmail.com>
2021-04-16 16:31:02 -0700

    adding services to use daos

--- services/questions-service.js
+++ services/questions-service.js
@@ -1,33 +1,5 @@
-const questions = require("./questions.json")
-const questionsModel = require("../models/questions/questions-model.js")
-
-const createQuestion = () => {}
-const createQuestionForQuiz = () => {}
-
-const findAllQuestions = () => {
-    return questionsModel.find()
-    //return questions
-}
-
-const findQuestionsForQuiz = (qzid) => {
-    return questionsModel.find({quizId: qzid})
-//    return questions.filter((question) => {
-//        return question.quizId === qzid;
-//    })
-}
-const findQuestionById = (quid) => {
-    return questionsModel.findById(quid)
-//    return questions.find((question) => {
-//        return question._id === quid;
-//    })
-}
-
-const updateQuestion = () => {}
-const deleteQuestion = () => {}
-
-module.exports = {
-    createQuestion, createQuestionForQuiz,
-    findAllQuestions, findQuestionById,
-    findQuestionsForQuiz,
-    updateQuestion, deleteQuestion
-}
+const questionsDao = require('../daos/questions-dao')
+const findAllQuestions = () => questionsDao.findAllQuestions()
+const findQuestionById = (qid) => questionsDao.findQuestionById(qid)
+const findQuestionsForQuiz = (qid) => questionsDao.findQuestionsForQuiz(qid)
+module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
--- services/quiz-service.js
+++ services/quiz-service.js
@@ -1,23 +1,4 @@
-const quizzes = require("./quizzes.json")
-
-const quizzesModel = require("../models/quizzes/quizzes-model")
-
-const createQuiz = () => {}
-const findAllQuizzes = () => {
-    return quizzesModel.find()
-    //return quizzes
-}
-const findQuizById = (qid) => {
-    return quizzesModel.findById(qid)
-//    return quizzes.find((quiz) => {
-//        return (quiz._id === qid)
-//    })
-}
-const updateQuiz = () => {}
-const deleteQuiz = () => {}
-
-module.exports = {
-    createQuiz,
-    findAllQuizzes, findQuizById,
-    updateQuiz, deleteQuiz
-}
+const quizzesDao = require('../daos/quizzes-dao')
+const findAllQuizzes = () => quizzesDao.findAllQuizzes()
+const findQuizById = (qid) => quizzesDao.findQuizById(qid)
+module.exports = { findAllQuizzes, findQuizById }
