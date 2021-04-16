70609bde9e768fba3148dc1aa2fca33393801dc3
Eshwari Bhide <ebhide99@gmail.com>
2021-04-16 16:34:42 -0700

    adding controllers

--- controllers/question-controller.js
+++ controllers/question-controller.js
@@ -1,20 +1,13 @@
-module.exports = (app) => {
-    const questionService = require("../services/questions-service")
-
-    const findAllQuestions = (req, res) => {
-//        const questions = questionService.findAllQuestions()
-//        res.send(questions)
-    questionService.findAllQuestions().then((questions => {res.send(questions)}))
-    }
-
-    const findQuestionsForQuiz = (req, res) => {
-    questionService.findQuestionsForQuiz(quizId).then((questions) => {res.send(questions)})
-
-//        const quizId = req.params.qid;
-//        const questions = questionService.findQuestionsForQuiz(quizId);
-//        res.send(questions);
-    }
-
-    app.get("/api/questions", findAllQuestions);
-    app.get("/api/quizzes/:qid/questions", findQuestionsForQuiz);
+const questionsService = require('../services/questions-service)
+module.exports = function(app) {
+   app.get('/api/quizzes/:qid/questions', (req, res) =>
+       questionsService.findQuestionsForQuiz(req.params['qid'])
+           .then(questions => res.json(questions)))
+   app.get('/api/questions', (req, res) =>
+       questionsService.findAllQuestions()
+           .then(allQuestions => res.json(allQuestions)))
+   app.get('/api/questions/:qid', (req, res) =>
+       questionsService.findQuestionById(req.params['qid'])
+           .then(question => res.json(question)))
 }
+
