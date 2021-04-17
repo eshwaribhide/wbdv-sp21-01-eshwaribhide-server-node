67f3f40d822b72a5b88f473d9e54b10b97c17dd8
Eshwari Bhide <ebhide99@gmail.com>
2021-04-16 18:32:21 -0700

    fixing issue

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 require('./controllers/quizzes-controller')(app)
-require('./controllers/questions-controller')(app)
+require('./controllers/question-controller')(app)
 require('./controllers/quiz-attempts-controller')(app)
 
 
